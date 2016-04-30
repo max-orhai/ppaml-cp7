5e561b4495747717d4d7909027d69be9f9c57c21
Michael Hueschen <mhueschen@galois.com>
2016-04-29 18:29:50 -0700

    Remove log directory argument

---
+++ Evaluator/eval.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+ABSOLUTE_PATH=$(cd `dirname "${BASH_SOURCE[0]}"` && pwd)
+
+if [ "$#" -ne 3 ]; then
+    echo "usage: $(basename $0) <run_out> <eval_in> <eval_output_dir>"
+    exit 1
+fi
+
+export PYTHONPATH=$ABSOLUTE_PATH
+
+run_out=$1
+eval_in=$2
+eval_output_dir=$3
+
+[ -d $eval_output_dir ] || mkdir $eval_output_dir
+
+python Evaluation.py $1 $2 $3
